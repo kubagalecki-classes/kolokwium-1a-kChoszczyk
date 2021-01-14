9930656c438ade9870ebe9429e2a1d4a79176a73
kChoszczyk <krzysiex97@gmail.com>
2021-01-14 12:28:51 +0000

    Zad 4 wer 1

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -5,5 +5,25 @@
 // tutaj funkcja obliczMake
 obliczMake(const std::vector<Tagliatelle>& vec)
 {
-  for(const Tagliatelle& e : vec)
+  double M = 0;
+  int num = 0;
+  for(auto it = vec.rend(); it!=vec.rend(); it++)
+  {
+    num++;
+    M+=it.ileMaki(num);
+  }
+  if(M>100)
+  {
+    int ex=1324;
+    throw ex;
+  }
+  else if(M>50)
+  {
+    double ex = 124.14324;
+    throw ex;
+  }
+  else
+  {
+    return M;
+  }
 }
