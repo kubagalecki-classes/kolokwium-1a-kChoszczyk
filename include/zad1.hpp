0d9a6cc8242eaad8b207181af26bb14fb44f3acb
kChoszczyk <krzysiex97@gmail.com>
2021-01-14 11:34:07 +0000

    Zadanie 1 pierwsza iteracja

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -4,5 +4,24 @@
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
 
+
 // Zad1
 // tutaj definicja klasy Tagliatelle
+struct Tagliatelle
+{
+  Tagliatelle(){L = 0.5; W = 0.5; R = 0.5;};
+  Tagliatelle(double x, double y, double z) : L(x), W(y), R(z) {};
+  
+  double ileMaki(unsigned P) const
+  {
+    double masa = P*L*W*(1. - R) * C;
+    return masa;
+  }
+
+  private:
+  double L;
+  double W;
+  double R;
+  static const double C;
+
+};
