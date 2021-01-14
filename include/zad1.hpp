ae50dcc055012f933caea48cf5d88af1ad578980
kChoszczyk <krzysiex97@gmail.com>
2021-01-14 11:49:50 +0000

    Zad 2 wersja 1

--- include/zad1.hpp
+++ include/zad1.hpp
@@ -3,16 +3,31 @@
 // Zad2
 // tutaj definicja klasy Makaron
 // BEZ definicji metody gotujMakaron (tylko z deklaracją)
-
+struct Makaron
+{
+  virtual double ileMaki(unsigned) const = 0;
+  static Makaron* gotujMakaron(const std::string& T)
+  {
+    if(T.front == T.back)
+    {
+      auto ret = new Tagliatelle{3.14, 0.42, 0.1};
+    }
+    else
+    {
+      auto ret = new Penne{};
+    }
+    return ret;
+  }
+};
 
 // Zad1
 // tutaj definicja klasy Tagliatelle
-struct Tagliatelle
+struct Tagliatelle : public Makaron
 {
   Tagliatelle(){L = 0.5; W = 0.5; R = 0.5;};
   Tagliatelle(double x, double y, double z) : L(x), W(y), R(z) {};
   
-  double ileMaki(unsigned P) const
+  double ileMaki(unsigned P) const override
   {
     double masa = P*L*W*(1. - R) * C;
     return masa;
--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,11 @@
 #include <type_traits>
 
 // tutaj klasa Penne
-
+struct Penne : public Makaron
+{
+  double ileMaki(unsigned P) const override
+  {
+    return P;
+  }
+};
 // tutaj definicja metody gotujMakaron
